3acaa924042b8f02ceb9d2e9ad3121e4956a4769
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-15 19:26:15 +0600

    comment / admin.py

--- apps/comment/admin.py
+++ apps/comment/admin.py
@@ -1,3 +1,14 @@
+from tokenize import Comment
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user", "content", "created_at", "is_active")
+    list_filter = ("is_active", "created_at")
+    search_fields = ("user__username", "content")
+    ordering = ("-created_at",)
+    fieldsets = (
+        (None, {"fields": ("user", "content", "parent")}),
+        ("Status", {"fields": ("is_active",)}),
+    )

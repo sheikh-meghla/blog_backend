1f6fca6b5dbbcff4f81fe59b5395ac43813767f4
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-15 18:53:06 +0600

    create views.py and instll rest_framework_simplejwt.authentication

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,83 @@
-from django.shortcuts import render
+from .models import CustomUser
+from rest_framework.exceptions import ValidationError
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.validators import ValidationError
+
+from .serializers import (
+    SignUpSerializer,
+    SignInSerializer,
+    SignOutSerializer,
+    ChangePasswordSerializer,
+
+)
+
 
 # Create your views here.
+class SignUpAPIView(APIView):
+    permission_classes = []
+
+    def post(self, request):
+
+        serializer = SignUpSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save()
+
+            return Response({
+                "status" : "success",
+                "message" : "User registered successfully.",
+                "data" : serializer.data
+            })
+        raise ValidationError(serializer.errors)
+
+class SignInAPIView(APIView):
+
+    permission_classes = []
+
+    def post(self, request):
+        
+        serializer = SignInSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            return Response({
+                "status" : "success",
+                "message" : "User signed in successfully.",
+                "data" : serializer.data
+            })
+        raise ValidationError(serializer.errors)
+
+
+class SignOutAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        serializer = SignOutSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+
+            return Response({
+                "status" : "success",
+                "message" : "Sign Out Successfull"
+            })
+        
+        raise ValidationError(serializer.errors)
+
+class ChangePasswordAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request):
+        serializer = ChangePasswordSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                "status" : "success",
+                "message" : "Password changed successfully.",
+                "data" : serializer.data
+            })
+        raise ValidationError(serializer.errors)

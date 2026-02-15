576f744f4117c827e32086c9fd1a6810c8245139
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-15 18:25:37 +0600

    create admin.py

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import CustomUser
+
+@admin.register(CustomUser)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "groups", "user_permissions")}),
+    )
+    search_fields = ("email",)
+
+

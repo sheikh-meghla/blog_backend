32f927253d56b91f043e6e7989febf7d8e937996
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-15 19:20:36 +0600

    settings

--- project/settings.py
+++ project/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.user',
+    'apps.comment'
 
     'rest_framework',
     'rest_framework_simplejwt',

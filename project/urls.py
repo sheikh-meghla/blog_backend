1277c49c2250214333fb6c7a58021a1712b12e66
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-16 15:01:14 +0600

    setting

--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/user/", include("apps.user.urls")),
+    path("api/blog/", include("apps.blog_post.urls")),
     
 ]

2ddd0f4e36048821ade8e1fc5ee383f8cc36d3fb
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-16 15:32:45 +0600

    comment/ursl.py

--- apps/comment/urls.py
+++ apps/comment/urls.py
@@ -6,3 +6,7 @@ urlpatterns = [
     path("comments/<int:pk>/", CommentDetailAPIView.as_view(), name="comment-detail"),
     
 ]
+# only one comment list ar url "http://127.0.0.1:8000/api/comments/comments/1/"
+# comment detail ar url "http://127.0.0.1:8000/api/comments/comments/123/"
+# comment create ar url "http://127.0.0.1:8000/api/comments/comments/1/"
+

aef12d26ea2be49915c485222b8dcacfaeaa0316
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-16 14:12:19 +0600

    blog_post/ admin.py

--- apps/blog_post/admin.py
+++ apps/blog_post/admin.py
@@ -4,9 +4,6 @@ from unfold.admin import ModelAdmin
 
 @admin.register(Post)
 class PostAdmin(ModelAdmin):
-    exclude = ('author',)
 
-    def save_model(self, request, obj, form, change):
-        if not obj.author_id:
-            obj.author = request.user
-        super().save_model(request, obj, form, change)
+    list_display = ("title", "author","is_published")
+    prepopulated_fields = {"slug": ("title",)}    

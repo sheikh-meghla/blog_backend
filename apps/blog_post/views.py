f9dbb9c7160696cb0b5d46f9b8d456c95049855e
sheikh-meghla <sheikh.meghla.dev@gmail.com>
2026-02-16 14:51:52 +0600

    blog_post/ views.py

--- apps/blog_post/views.py
+++ apps/blog_post/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostListAPIView(generics.ListAPIView):
+    queryset = Post.objects.filter(is_published=True)
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny] 
+
+class PostDetailAPIView(generics.RetrieveAPIView):
+    queryset = Post.objects.filter(is_published=True)
+    serializer_class = PostSerializer
+    permission_classes = [permissions.AllowAny]
+    lookup_field = 'slug' 
+
+class PostCreateAPIView(generics.CreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAdminUser] 
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
